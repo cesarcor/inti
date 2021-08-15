ae26d7d87f82e52247ee02f13294709fca5d29de
cesarcor <cesarcorrechel@gmail.com>
2021-08-15 17:59:08 -0600

    adjust header layout

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,28 +1,26 @@
-import React from "react";
-import styles from "./header.module.scss";
-import SearchForm from "../search-form/search-form";
-import Menu from "../menu/menu";
-import ActionMenu from "../action-menu/action-menu";
+import React from 'react';
+import styles from './header.module.scss';
+import SearchForm from '../search-form/search-form';
+import Menu from '../menu/menu';
+import ActionMenu from '../action-menu/action-menu';
 
 const Header = () => {
   return (
     <header className={styles.site_header}>
       <div className={styles.upper_site_header}>
         <div className={styles.container}>
-          <div className={styles.header_form}>
+          <div className={styles.header_form_col}>
             <SearchForm />
           </div>
-          <div className={styles.logo}>
-            LOGO
-          </div>
-          <div className={styles.action_menu}>
+          <div className={styles.logo_col}>LOGO</div>
+          <div className={styles.action_menu_col}>
             <ActionMenu />
           </div>
         </div>
       </div>
       <div className={styles.lower_site_header}>
         <div className={styles.container}>
-          <div className={styles.header_form}>
+          <div className={styles.header_form_col}>
             <SearchForm />
           </div>
           <div>
