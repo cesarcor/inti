d7609d4ed7555b1a68fb7930b26d82338c7216e0
cesarcor <cesarcorrechel@gmail.com>
2021-08-15 17:41:35 -0600

    add icons

--- src/components/action-menu/action-menu.jsx
+++ src/components/action-menu/action-menu.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './action-menu.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle, faHeart, faShoppingBag } from '@fortawesome/free-solid-svg-icons';
 
 const ActionMenu = () => {
     return (
         <div className={styles.action_menu}>
           <ul className={styles.action_menu__list}>
-            <li><a href="#">A</a></li>
-            <li><a href="#">W</a></li>
-            <li><a href="#">C</a></li>
+            <li><a href="#"><FontAwesomeIcon icon={faUserCircle} /></a></li>
+            <li><a href="#"><FontAwesomeIcon icon={faHeart} /></a></li>
+            <li><a href="#"><FontAwesomeIcon icon={faShoppingBag} /></a></li>
           </ul>  
         </div>
     )
