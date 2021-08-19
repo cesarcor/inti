ec1e5c9e7ec2ed3a2dbee2aa50d512e888edaa46
cesarcor <cesarcorrechel@gmail.com>
2021-08-18 23:20:31 -0600

    add submit btn + icons

--- src/components/search-form/search-form.jsx
+++ src/components/search-form/search-form.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from './search-form.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const SearchForm = () => {
     return (
         <form className={styles.search_form}>
             <input type="text" placeholder="what are you looking for?" />
+            <button type="submit" className={styles.submit_btn}><FontAwesomeIcon icon={faSearch} /></button>
         </form>
     )
 }
