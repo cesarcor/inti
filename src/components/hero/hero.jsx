3edb4264a65401618a099be653b5858cebca9235
cesarcor <cesarcorrechel@gmail.com>
2021-08-29 00:15:01 -0600

    adjust content spacing and positioning

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import styles from "./hero.module.scss";
-import heroImg from "../../assets/images/tempimg1.jpg"
+import heroImg from "../../assets/images/tempimg1.jpg";
+import Button from "../button/button";
 
 const Hero = () => {
   return (
     <div className={styles.hero}>
-        <div className={styles.hero_content_col}>
-            <h1>Mega Planters</h1>
-            <p>Lorem ipsum dolor sit amet, consetur sapipang individunt pakisan</p>
+      <div className={styles.hero_content_col}>
+        <div className={styles.content_container}>
+          <h1>Mega Planters</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consetur sapipang individunt pakisan
+          </p>
+          <Button btn_text="Shop Now" />
         </div>
-        <div className={styles.hero_image_col}>
-            <figure>
-             <img src={heroImg} />
-            </figure>
-        </div>
+      </div>
+      <div className={styles.hero_image_col}>
+        <figure>
+          <img src={heroImg} />
+        </figure>
+      </div>
     </div>
   );
 };
