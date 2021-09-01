5debe8bff89446eb989cf8def71bb6c91ed1ec6b
cesarcor <cesarcorrechel@gmail.com>
2021-09-01 00:19:13 -0600

    add product carousel comp.

---
+++ src/components/product-carousel/product-carousel.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import styles from "./product-carousel.module.scss";
+import ProductItem from "../product-item/product-item";
+
+const ProductCarousel = () => {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 6000, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 4,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  return (
+    <div>
+      <Carousel
+        responsive={responsive}
+        itemClass={styles.product_carousel__item}
+      >
+        <ProductItem product_name="Lorem Ipsum" price="399" />
+        <ProductItem product_name="Lorem Ipsum" price="399" />
+        <ProductItem product_name="Lorem Ipsum" price="399" />
+        <ProductItem product_name="Lorem Ipsum" price="399" />
+        <ProductItem product_name="Lorem Ipsum" price="399" />
+        <ProductItem product_name="Lorem Ipsum" price="399" />
+        <ProductItem product_name="Lorem Ipsum" price="399" />
+        <ProductItem product_name="Lorem Ipsum" price="399" />
+      </Carousel>
+    </div>
+  );
+};
+
+export default ProductCarousel;
