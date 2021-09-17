6a2648991277cfade17b141060d69d932a0d9ef5
cesarcor <cesarcorrechel@gmail.com>
2021-09-17 11:21:44 -0600

    add menu & credit

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import styles from './footer.module.scss';
+import React from "react";
+import styles from "./footer.module.scss";
+import FooterMenu from "../footer-menu/footer-menu";
 
 const Footer = () => {
-    return (
-        <footer className={styles.site_footer}>
-            <div className={styles.footer_container}>
-                <div className={styles.col_1}>
-                    <h1>Inti</h1>
-                </div>
-                <div className={styles.col_2}>
-                    
-                </div>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className={styles.site_footer}>
+      <div className={styles.footer_container}>
+        <div className={styles.col_1}>
+          <h1>Inti</h1>
+        </div>
+        <div className={styles.col_2}>
+          <FooterMenu />
+        </div>
+        <div className={styles.col_3}>
+          <span>
+            Developed by <a href="#">Cesar Correchel</a>
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
