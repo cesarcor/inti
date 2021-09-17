5e33db7ec1a84acd5a2789d243997a3b17be5946
cesarcor <cesarcorrechel@gmail.com>
2021-09-17 12:01:04 -0600

    add mobile menu

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
-import styles from './menu.module.scss';
+import React from "react";
+import styles from "./menu.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Menu = () => {
-    return (
-        <nav className={styles.site_menu}>
-            <ul className={styles.site_menu__list}>
-                <li><a href="#">Flower Pot</a></li>
-                <li><a href="#">Planter</a></li>
-                <li><a href="#">Pillars</a></li>
-                <li><a href="#">Silk Flowers</a></li>
-            </ul>
-            <button className={styles.burger_menu}>Menu</button>
-        </nav>
-    )
-}
+  return (
+    <nav className={styles.site_menu}>
+      <ul className={styles.site_menu__list}>
+        <li>
+          <a href="#">Flower Pot</a>
+        </li>
+        <li>
+          <a href="#">Planter</a>
+        </li>
+        <li>
+          <a href="#">Pillars</a>
+        </li>
+        <li>
+          <a href="#">Silk Flowers</a>
+        </li>
+      </ul>
+      <button className={styles.burger_menu}>
+        <span>Menu</span>
+        <FontAwesomeIcon icon={faBars} />
+      </button>
+    </nav>
+  );
+};
 
 export default Menu;
