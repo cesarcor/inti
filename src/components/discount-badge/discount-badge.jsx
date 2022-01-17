417f06bf5c6f485aff9af37dbdc7a99f19bae6ef
cesarcor <cesarcorrechel@gmail.com>
2022-01-16 22:13:05 -0600

    add discount badge

---
+++ src/components/discount-badge/discount-badge.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './discount-badge.module.scss';
+
+const DiscountBadge = (props) => {
+  return (
+    <div className={styles.discount_badge}>
+      <span className={styles.discount_badge__content}>
+        <span className={styles.discount_badge__amount}>15%</span>
+        <span className={styles.discount_badge__title}>discount</span>
+      </span>
+    </div>
+  );
+};
+
+export default DiscountBadge;
