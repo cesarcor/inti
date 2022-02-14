a54b5379104f56b80d01acd9617b1b51a351d5ff
cesarcor <cesarcorrechel@gmail.com>
2022-02-13 21:19:12 -0600

    add svg underline

--- src/components/heading/heading.jsx
+++ src/components/heading/heading.jsx
@@ -1,8 +1,16 @@
-import React from "react";
-import styles from "./heading.module.scss";
+import React from 'react';
+import styles from './heading.module.scss';
+import { ReactComponent as GreenUnderline } from '../../assets/svg/green-underline.svg';
 
 const Heading = (props) => {
-  return <h2 className={styles.heading}>{props.text}</h2>;
+  return (
+    <h2 className={styles.heading}>
+      <span>{props.text}</span>
+      <span>
+        <GreenUnderline />
+      </span>
+    </h2>
+  );
 };
 
 export default Heading;
