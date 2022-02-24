0a99adf4ba0047b6a70c641b60f2d16c750269a1
cesarcor <cesarcorrechel@gmail.com>
2022-02-23 22:51:33 -0600

    add variation

--- src/components/discount-badge/discount-badge.jsx
+++ src/components/discount-badge/discount-badge.jsx
@@ -3,7 +3,13 @@ import styles from './discount-badge.module.scss';
 
 const DiscountBadge = (props) => {
   return (
-    <div className={styles.discount_badge}>
+    <div
+      className={
+        props.discount_badge_type === 'big'
+          ? `${styles.discount_badge_big} ${styles.discount_badge}`
+          : `${styles.discount_badge_small} ${styles.discount_badge}`
+      }
+    >
       <span className={styles.discount_badge__content}>
         <span className={styles.discount_badge__amount}>15%</span>
         <span className={styles.discount_badge__title}>discount</span>
