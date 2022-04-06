eee2e36f6e724737485086a56d97344073eff4c4
cesarcor <cesarcorrechel@gmail.com>
2022-04-05 23:23:29 -0600

    add numbered indicator to cart

--- src/components/action-menu/action-menu.jsx
+++ src/components/action-menu/action-menu.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import styles from './action-menu.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle, faHeart, faShoppingBag } from '@fortawesome/free-solid-svg-icons';
+import {
+  faUserCircle,
+  faHeart,
+  faShoppingBag,
+} from '@fortawesome/free-solid-svg-icons';
 
 const ActionMenu = () => {
-    return (
-        <div className={styles.action_menu}>
-          <ul className={styles.action_menu__list}>
-            <li><a href="#"><FontAwesomeIcon icon={faUserCircle} /></a></li>
-            <li><a href="#"><FontAwesomeIcon icon={faHeart} /></a></li>
-            <li><a href="#"><FontAwesomeIcon icon={faShoppingBag} /></a></li>
-          </ul>  
-        </div>
-    )
-}
+  return (
+    <div className={styles.action_menu}>
+      <ul className={styles.action_menu__list}>
+        <li>
+          <a href='#'>
+            <FontAwesomeIcon icon={faUserCircle} />
+          </a>
+        </li>
+        <li>
+          <a href='#'>
+            <FontAwesomeIcon icon={faHeart} />
+          </a>
+        </li>
+        <li>
+          <a href='#'>
+            <FontAwesomeIcon icon={faShoppingBag} />
+            <span className={styles.cart_number}>2</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default ActionMenu;
