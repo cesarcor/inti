9c2a880bdda796460a90539797dd27fc5caf1e38
cesarcor <cesarcorrechel@gmail.com>
2022-05-03 22:40:28 -0600

    add topbar to header comp.

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,34 +1,38 @@
-import React from 'react';
+import { React, Fragment } from 'react';
 import styles from './header.module.scss';
 import SearchForm from '../search-form/search-form';
 import Menu from '../menu/menu';
 import ActionMenu from '../action-menu/action-menu';
+import TopBanner from '../top-banner/top-banner';
 
 const Header = () => {
   return (
-    <header className={styles.site_header}>
-      <div className={styles.upper_site_header}>
-        <div className={styles.container}>
-          <div className={styles.header_form_col}>
-            <SearchForm />
-          </div>
-          <div className={styles.logo_col}>LOGO</div>
-          <div className={styles.action_menu_col}>
-            <ActionMenu />
-          </div>
-        </div>
-      </div>
-      <div className={styles.lower_site_header}>
-        <div className={styles.container}>
-          <div className={styles.header_form_col}>
-            <SearchForm />
-          </div>
-          <div className={styles.menu_col}>
-            <Menu />
+    <Fragment>
+      <TopBanner />
+      <header className={styles.site_header}>
+        <div className={styles.upper_site_header}>
+          <div className={styles.container}>
+            <div className={styles.header_form_col}>
+              <SearchForm />
+            </div>
+            <div className={styles.logo_col}>LOGO</div>
+            <div className={styles.action_menu_col}>
+              <ActionMenu />
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+        <div className={styles.lower_site_header}>
+          <div className={styles.container}>
+            <div className={styles.header_form_col}>
+              <SearchForm />
+            </div>
+            <div className={styles.menu_col}>
+              <Menu />
+            </div>
+          </div>
+        </div>
+      </header>
+    </Fragment>
   );
 };
 
