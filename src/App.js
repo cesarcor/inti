cd7169aa93af29ca17ff9d906e98c0387b6013ea
cesarcor <cesarcorrechel@gmail.com>
2022-06-20 21:02:13 -0600

    fix React Router scroll location problem

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import Page from './routes/page/page';
 import Home from './routes/home/home.jsx';
 import Category from './routes/category/category';
 import SingleProduct from './routes/single-product/single-product.jsx';
+import ScrollToTop from './utils/scroll-to-top';
 
 function App() {
 	return (
-		<Routes>
-			<Route path='/' element={<Page />}>
-				<Route index element={<Home />} />
-				<Route path='/category/:catid' element={<Category />} />
-				<Route path='/product/:prodid' element={<SingleProduct />} />
-			</Route>
-		</Routes>
+		<ScrollToTop>
+			<Routes>
+				<Route path='/' element={<Page />}>
+					<Route index element={<Home />} />
+					<Route path='/category/:catid' element={<Category />} />
+					<Route path='/product/:prodid' element={<SingleProduct />} />
+				</Route>
+			</Routes>
+		</ScrollToTop>
 	);
 }
 
